00d0842bfafa5f0eceedbc6cc3aa2e2bf3caa3f6
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-04 23:34:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,14 @@
 class Dog 
   def initialize(name,breed)
     @name= name
+    @breed= breed
   end
   def name=(name)
     @name=name
+    @breed=breed
   end
   def name
     @name
+    @breed
   end
 end
